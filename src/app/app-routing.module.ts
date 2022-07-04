296d2bfe29eff8d045fffff75857121f5a050484
M-jithendra-suryavarma <suriyamp7550@gmail.com>
2022-07-04 06:48:08 +0530

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PeopleComponent } from './people/people.component';
+import {AppComponent} from './app.component'
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
-  
+  {path:'', component: AppComponent},
+  {path:'/people', component: PeopleComponent},
+  {path:'/hotels', component: LandingPageComponent},
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes,{enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
