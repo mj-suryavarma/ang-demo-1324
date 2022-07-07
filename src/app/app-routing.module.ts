914ee5c0c5a0ccc881f73f2472fb86f9db7844f6
M-jithendra-suryavarma <suriyamp7550@gmail.com>
2022-07-07 06:37:09 +0530

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PeopleComponent } from './people/people.component';
-import {AppComponent} from './app.component'
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+
 
 const routes: Routes = [
-  {path:'', component: AppComponent},
-  {path:'/people', component: PeopleComponent},
-  {path:'/hotels', component: LandingPageComponent},
+  {path:'people', component: PeopleComponent},
+  {path:'hotels', component: LandingPageComponent},
+  {path:'login', component: LoginPageComponent},
+  {path: '', redirectTo: "/login", pathMatch:'full' }
 
 ];
 
 @NgModule({
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(routes,{enableTracing: true})],
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PeopleComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
